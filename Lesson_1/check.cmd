2f28bb16c1c7ab4064d5b202190611ae838b1df6
Inna <meerenns@gmail.com>
2026-02-09 22:15:22 -0700

    Cleanup repository

--- Lesson_1/check.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-"C:\Program Files\Java\jdk-21.0.10\bin\java.exe" ^
-  -Dfile.encoding=UTF-8 ^
-  -jar "C:\Users\Chaser\Desktop\StartJava\utils\checkstyle-13.1.0-all.jar" ^
-  -c "C:\Users\Chaser\Desktop\StartJava\startjava_checks.xml" ^
-  %*
