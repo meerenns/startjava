774cb2a819425cfce513fe491d62b2931142a593
Inna <meerenns@gmail.com>
2026-04-05 19:01:56 -0600

    Fix GuessNumberGame

--- Lesson_1/GuessNumberGame.java
+++ Lesson_1/GuessNumberGame.java
@@ -3,24 +3,24 @@ import java.util.Random;
 public class GuessNumberGame {
     public static void main(String[] args) {
         Random random = new Random();
-        int luckyNumber = random.nextInt(1, 101);
+        int secretNum = random.nextInt(1, 101);
 
         int left = 1;
         int right = 100;
-        int guess;
+        int guess = (left + right) / 2;
 
-        while (true) {
-            guess = (left + right) / 2;
-            if (guess > luckyNumber) {
+        while (guess != secretNum) {
+            if (guess > secretNum) {
                 System.out.println(guess + " больше того, что загадал компьютер");
                 right = guess - 1;
-            } else if (guess < luckyNumber) {
+            } else if (guess < secretNum) {
                 System.out.println(guess + " меньше того, что загадал компьютер");
                 left = guess + 1;
-            } else {
-                System.out.println("Вы победили!");
-                break;
             }
+
+            guess = (left + right) / 2;
         }
+
+        System.out.println("Вы победили!");
     }
 }
